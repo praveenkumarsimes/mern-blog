da12c430ba13e80f45b06c09dbb04efc7241131f
manikandansitpl <manikandansitpl3@gmail.com>
2024-02-14 16:17:08 +0530

    first commit

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,7 +5,7 @@ import jwt from 'jsonwebtoken';
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-
+   console.log(username,email,password);
   if (
     !username ||
     !email ||
--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  apiKey: "AIzaSyACZhL0f8wcco_R1wWgfXP8PxjmyC0XoU4",
+  authDomain: "myblog-e8a4e.firebaseapp.com",
+  projectId: "myblog-e8a4e",
+  storageBucket: "myblog-e8a4e.appspot.com",
+  messagingSenderId: "640500658361",
+  appId: "1:640500658361:web:94100d4d2ff9365823eb68"
 };
 
 // Initialize Firebase

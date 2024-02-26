a0f06903192cb8416b2fb29857958e62887d5087
manikandansitpl <manikandansitpl3@gmail.com>
2024-02-27 02:48:50 +0530

    true

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -58,7 +58,7 @@ export const signin = async (req, res, next) => {
 
     res
       .status(200)
-      .cookie('access_token', token,{httpOnly: false})
+      .cookie('access_token', token,{httpOnly: true})
       .json(rest);
   } catch (error) {
     next(error);

f0b42ddf18accf2f1d6f634af2392988542d8f79
manikandansitpl <manikandansitpl3@gmail.com>
2024-02-27 02:58:53 +0530

    app change

--- api/index.js
+++ api/index.js
@@ -10,6 +10,7 @@ import path from 'path';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 
+const app = express();
 dotenv.config();
 
 mongoose.set('strictQuery',false);
@@ -38,7 +39,7 @@ dBConnection().then(() => {
 });
 
 
-const app = express();
+
 
 app.use(express.json());
 app.use(cookieParser());

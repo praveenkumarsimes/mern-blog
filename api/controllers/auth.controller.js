7f3b84fff96b7c7e75122ddf037c9102940f7509
manikandansitpl <manikandansitpl3@gmail.com>
2024-02-27 03:26:36 +0530

    domain change

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -103,7 +103,7 @@ export const google = async (req, res, next) => {
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
-        .cookie('access_token', token,{httpOnly: false})
+        .cookie("access_token", token, { domain: 'https://www.thaimeera.com/' })
         .json(rest);
     }
   } catch (error) {

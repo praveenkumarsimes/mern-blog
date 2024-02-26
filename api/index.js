b81c72fafa5a6215cc71007f0496c4ae3704f6c0
manikandansitpl <manikandansitpl3@gmail.com>
2024-02-26 19:54:11 +0530

    .env remove

--- api/index.js
+++ api/index.js
@@ -14,7 +14,7 @@ dotenv.config();
 
 const dBConnection =async()=>{
  await mongoose
-  .connect(process.env.MONGO_URI)
+  .connect("mongodb+srv://user123:XyLkuCWFErDM8MLj@cluster0.f8eue.mongodb.net/?retryWrites=true&w=majority")
   .then(() => {
     console.log('MongoDb is connected');
   })
